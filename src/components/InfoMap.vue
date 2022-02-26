1592096aa67bd78b0aa1a5ef672d8761b694b700
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2022-02-27 01:21:57 +0200

    feat: add basic empty map

--- src/components/InfoMap.vue
+++ src/components/InfoMap.vue
@@ -1,3 +1,36 @@
 <template>
-	<div class="p-4 bg-yellow-100 rounded">Info Map</div>
+	<div ref="mapRef" class="h-full" />
 </template>
+
+<script setup lang="ts">
+import { View, Map } from "ol";
+import { Tile } from "ol/layer";
+import { OSM } from "ol/source";
+import { onMounted, ref } from "vue";
+
+const mapRef = ref<HTMLElement>();
+
+onMounted(() => {
+	if (!mapRef.value) return;
+
+	const source = new OSM();
+	const layer = new Tile({ source });
+	const view = new View({
+		projection: "EPSG:3857",
+		center: [3167475.240184986, 5952086.044611719],
+		extent: [
+			2437181.208848241, 5100607.633807939, 3893306.0536067626, 6903564.4554155,
+		],
+		zoom: 7,
+		minZoom: 5,
+	});
+
+	const map = new Map({
+		target: mapRef.value,
+		// Removes all default controls
+		controls: [],
+		layers: [layer],
+		view,
+	});
+});
+</script>
