b8b09765a4f32894b3502a81d84665d0ea5c3b75
Sergiu <synthezys@gmail.com>
2022-02-26 23:51:52 +0200

    feat: header section

---
+++ src/components/Base/BaseButton.vue
@@ -0,0 +1,36 @@
+<template>
+	<button @click="$emit('click')" :class="cssStyles">
+		<slot />
+	</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, computed } from "vue";
+
+export default defineComponent({
+	name: "BaseButton",
+	props: {
+		color: {
+			type: String as PropType<"blue" | "white-outline" | "red">,
+			default: "border-white",
+		},
+	},
+	emits: ["click"],
+	setup(props) {
+		const cssStyles = computed(() => {
+			switch (props.color) {
+				case "white-outline":
+					return "border-white";
+				case "red":
+					return "border-red-500";
+				default:
+					return "border border-white py-2 px-4 rounded text-white leading-none hover:bg-white hover:text-blue-500 hover:border-blue-500";
+			}
+		});
+
+		return {
+			cssStyles,
+		};
+	},
+});
+</script>
