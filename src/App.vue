ec73d66cd8469da7a7fc7e3ed19410c540c34312
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2022-02-28 01:22:50 +0200

    fix: render panes only once

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@ import InfoMap from "./components/InfoMap.vue";
 import Header from "./components/Header.vue";
 import AddButton from "./components/AddButton.vue";
 import TopTabs from "./components/TopTabs.vue";
+import { useMediaQuery } from "@vueuse/core";
+
+const isDesktop = useMediaQuery("(min-width: 1024px)");
 </script>
 
 <template>
@@ -11,18 +14,17 @@ import TopTabs from "./components/TopTabs.vue";
 		<Header />
 
 		<!-- TODO: Find a way to render a single time only -->
-		<div class="lg:hidden h-full">
-			<TopTabs>
-				<template #map>
-					<InfoMap />
-				</template>
-				<template #offers>
-					<List />
-				</template>
-			</TopTabs>
-		</div>
+		<!-- TODO: Move data to parent and load only if missing -->
+		<TopTabs v-if="!isDesktop">
+			<template #map>
+				<InfoMap />
+			</template>
+			<template #offers>
+				<List />
+			</template>
+		</TopTabs>
 
-		<div class="hidden lg:grid grid-cols-2 p-4 gap-4 flex-1">
+		<div v-if="isDesktop" class="grid grid-cols-2 p-4 gap-4 flex-1">
 			<List />
 			<InfoMap />
 		</div>
