5771769c7329af3276693bf53cd7dbbb657c8969
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2022-02-27 02:09:22 +0200

    feat: add map icons with basic functionality

--- src/components/InfoMap.vue
+++ src/components/InfoMap.vue
@@ -1,14 +1,59 @@
 <template>
-	<div ref="mapRef" class="h-full" />
+	<div class="relative">
+		<div ref="mapRef" class="h-full" />
+		<div class="absolute inset-y-0 flex flex-col justify-center gap-1 left-1">
+			<MapButton @click="zoom(0.5)">
+				<AddIcon class="w-4 h-4" />
+			</MapButton>
+			<MapButton @click="zoom(-0.5)">
+				<RemoveIcon class="w-4 h-4" />
+			</MapButton>
+			<MapButton @click="goHome">
+				<HomeIcon class="w-4 h-4" />
+			</MapButton>
+			<MapButton @click="locateMe">
+				<GpsFixedIcon class="w-4 h-4" />
+			</MapButton>
+			<MapButton>
+				<StraightenIcon class="w-4 h-4" />
+			</MapButton>
+			<MapButton>
+				<PolylineIcon class="w-4 h-4" />
+			</MapButton>
+			<MapButton>
+				<LayersIcon class="w-4 h-4" />
+			</MapButton>
+			<MapButton>
+				<ViewListIcon class="w-4 h-4" />
+			</MapButton>
+		</div>
+	</div>
 </template>
 
-<script setup lang="ts">
-import { View, Map } from "ol";
-import { Tile } from "ol/layer";
-import { OSM } from "ol/source";
-import { onMounted, ref } from "vue";
+<script lang="ts" setup>
+import { View, Map, Feature, Geolocation } from "ol";
+import { Tile, Vector as LayerVector } from "ol/layer";
+import { OSM, Vector as SourceVector } from "ol/source";
+import { Point } from "ol/geom";
+import { Style, Fill, Stroke, Icon } from "ol/style";
+import {
+	h,
+	onMounted,
+	ref,
+	FunctionalComponent,
+	ButtonHTMLAttributes,
+} from "vue";
+import AddIcon from "../icons/AddIcon.vue";
+import RemoveIcon from "../icons/RemoveIcon.vue";
+import HomeIcon from "../icons/HomeIcon.vue";
+import GpsFixedIcon from "../icons/GpsFixedIcon.vue";
+import StraightenIcon from "../icons/StraightenIcon.vue";
+import PolylineIcon from "../icons/PolylineIcon.vue";
+import LayersIcon from "../icons/LayersIcon.vue";
+import ViewListIcon from "../icons/ViewListIcon.vue";
 
 const mapRef = ref<HTMLElement>();
+let olMap: Map;
 
 onMounted(() => {
 	if (!mapRef.value) return;
@@ -25,12 +70,78 @@ onMounted(() => {
 		minZoom: 5,
 	});
 
-	const map = new Map({
+	olMap = new Map({
 		target: mapRef.value,
 		// Removes all default controls
 		controls: [],
-		layers: [layer],
+		layers: [layer, searchLayer],
 		view,
 	});
+});
+
+function zoom(delta: number) {
+	const zoom = olMap.getView().getZoom();
+	if (!zoom) return;
+	olMap.getView().animate({ zoom: zoom + delta, duration: 150 });
+}
+
+function goHome() {
+	olMap.getView().animate({
+		zoom: 7,
+		center: [3167475.240184986, 5952086.044611719],
+		duration: 400,
+	});
+}
+
+function addMeOnMap(position: number[] | undefined) {
+	if (!position) return;
+	const feature = new Feature({ geometry: new Point(position), type: "point" });
+	searchLayer.getSource().clear();
+	searchLayer.getSource().addFeature(feature);
+	olMap.getView().animate({ zoom: 12, center: position, duration: 400 });
+}
+
+function locateMe() {
+	const projection = olMap.getView().getProjection();
+	const geolocation = new Geolocation({ projection, tracking: true });
+	geolocation.once("change", () => addMeOnMap(geolocation.getPosition()));
+	addMeOnMap(geolocation.getPosition());
+}
+
+const MapButton: FunctionalComponent<ButtonHTMLAttributes> = (
+	props,
+	{ slots },
+) => {
+	const className = [
+		"bg-white rounded-lg border-2 border-blue-500 text-blue-500 p-1",
+		props.class,
+	];
+
+	const newProps = {
+		...props,
+		class: className,
+	};
+
+	return h("button", newProps, slots);
+};
+
+const searchLayer = new LayerVector({
+	source: new SourceVector(),
+	style: new Style({
+		fill: new Fill({
+			color: "rgba(41, 98, 255, 0.2)",
+		}),
+		stroke: new Stroke({
+			color: "#2962FF",
+			width: 3,
+		}),
+		image: new Icon({
+			src: "./assets/images/marker.png",
+			scale: 0.34,
+			anchor: [0.5, 1],
+			anchorXUnits: "fraction",
+			anchorYUnits: "fraction",
+		}),
+	}),
 });
 </script>
