7c96ec0ccb81ca316ab9ea3799321b62171fb3e1
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2022-02-27 17:38:35 +0200

    fix: remove unusd wrapper divs

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,101 +1,95 @@
 <template>
-	<div class="flex flex-col">
-		<div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-			<div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-				<div
-					class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"
-				>
-					<table class="min-w-full divide-y divide-gray-200">
-						<thead class="bg-gray-50">
-							<tr>
-								<th
-									scope="col"
-									class="cursor-pointer px-6 py-3 text-left text-xs hover:bg-blue-200 font-medium text-gray-600 uppercase tracking-wider bg-gray-200"
-								>
-									Название
-								</th>
-								<th
-									scope="col"
-									class="cursor-pointer px-6 py-3 text-left text-xs hover:bg-blue-200 font-medium text-gray-600 uppercase tracking-wider bg-gray-200"
-								>
-									Регион
-								</th>
-								<th
-									scope="col"
-									class="cursor-pointer px-6 py-3 text-left text-xs hover:bg-blue-200 font-medium text-gray-600 uppercase tracking-wider bg-gray-200"
-								>
-									Локация
-								</th>
-								<th
-									scope="col"
-									class="cursor-pointer px-6 py-3 text-left text-xs hover:bg-blue-200 font-medium text-gray-600 uppercase tracking-wider bg-gray-200"
-								>
-									Тип помощи
-								</th>
-								<th scope="col" class="relative px-6 py-3 bg-gray-200">
-									<span class="sr-only">Edit</span>
-								</th>
-							</tr>
-						</thead>
-						<tbody class="bg-white divide-y divide-gray-200">
-							<tr v-for="offer in offers" :key="offer.id">
-								<td class="px-6 py-4 whitespace-nowrap">
-									<div class="flex items-center">
-										<!--									<div class="flex-shrink-0 h-10 w-10">-->
-										<!--										<img class="h-10 w-10 rounded-full" :src="offer.image" alt="" />-->
-										<!--									</div>-->
-										<div>
-											<div class="text-sm font-medium text-gray-900">
-												{{ offer.name }}
-											</div>
-										</div>
+	<div>
+		<div class="shadow overflow-x-auto border-b border-gray-200 sm:rounded-lg">
+			<table class="min-w-full divide-y divide-gray-200">
+				<thead class="bg-gray-50">
+					<tr>
+						<th
+							scope="col"
+							class="cursor-pointer px-6 py-3 text-left text-xs hover:bg-blue-200 font-medium text-gray-600 uppercase tracking-wider bg-gray-200"
+						>
+							Название
+						</th>
+						<th
+							scope="col"
+							class="cursor-pointer px-6 py-3 text-left text-xs hover:bg-blue-200 font-medium text-gray-600 uppercase tracking-wider bg-gray-200"
+						>
+							Регион
+						</th>
+						<th
+							scope="col"
+							class="cursor-pointer px-6 py-3 text-left text-xs hover:bg-blue-200 font-medium text-gray-600 uppercase tracking-wider bg-gray-200"
+						>
+							Локация
+						</th>
+						<th
+							scope="col"
+							class="cursor-pointer px-6 py-3 text-left text-xs hover:bg-blue-200 font-medium text-gray-600 uppercase tracking-wider bg-gray-200"
+						>
+							Тип помощи
+						</th>
+						<th scope="col" class="relative px-6 py-3 bg-gray-200">
+							<span class="sr-only">Edit</span>
+						</th>
+					</tr>
+				</thead>
+				<tbody class="bg-white divide-y divide-gray-200">
+					<tr v-for="offer in offers" :key="offer.id">
+						<td class="px-6 py-4 whitespace-nowrap">
+							<div class="flex items-center">
+								<!--									<div class="flex-shrink-0 h-10 w-10">-->
+								<!--										<img class="h-10 w-10 rounded-full" :src="offer.image" alt="" />-->
+								<!--									</div>-->
+								<div>
+									<div class="text-sm font-medium text-gray-900">
+										{{ offer.name }}
 									</div>
-								</td>
-								<td class="px-6 py-4 whitespace-nowrap">
-									<div class="text-sm text-gray-900">{{ offer.region }}</div>
-								</td>
-								<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-									<div class="text-sm text-gray-500">{{ offer.location }}</div>
-								</td>
-								<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-									<div class="text-sm text-gray-500">{{ offer.helpType }}</div>
-								</td>
-								<td class="flex items-center text-sm py-4 px-6">
-									<a
-										v-if="offer.contacts.phone1"
-										:href="`tel:${offer.contacts.phone1}`"
-										class="text-blue-600 hover:text-blue-900"
-									>
-										<PhoneIcon class="h-5 w-5 text-blue-500" />
-									</a>
-									<a
-										v-if="offer.contacts.phone2"
-										:href="`tel:${offer.contacts.phone2}`"
-										class="text-blue-600 hover:text-blue-900"
-									>
-										<PhoneIcon class="h-5 w-5 text-blue-500" />
-									</a>
-									<a
-										v-if="offer.contacts.email"
-										:href="`mailto:${offer.contacts.phone1}`"
-										class="text-blue-600 hover:text-blue-900"
-									>
-										<MailIcon class="h-5 w-5 text-blue-500" />
-									</a>
-									<a
-										v-if="offer.contacts.facebook"
-										target="_blank"
-										:href="offer.contacts.facebook"
-										class="text-blue-600 hover:text-blue-900"
-									>
-										<IconFB />
-									</a>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-			</div>
+								</div>
+							</div>
+						</td>
+						<td class="px-6 py-4 whitespace-nowrap">
+							<div class="text-sm text-gray-900">{{ offer.region }}</div>
+						</td>
+						<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+							<div class="text-sm text-gray-500">{{ offer.location }}</div>
+						</td>
+						<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+							<div class="text-sm text-gray-500">{{ offer.helpType }}</div>
+						</td>
+						<td class="flex items-center text-sm py-4 px-6">
+							<a
+								v-if="offer.contacts.phone1"
+								:href="`tel:${offer.contacts.phone1}`"
+								class="text-blue-600 hover:text-blue-900"
+							>
+								<PhoneIcon class="h-5 w-5 text-blue-500" />
+							</a>
+							<a
+								v-if="offer.contacts.phone2"
+								:href="`tel:${offer.contacts.phone2}`"
+								class="text-blue-600 hover:text-blue-900"
+							>
+								<PhoneIcon class="h-5 w-5 text-blue-500" />
+							</a>
+							<a
+								v-if="offer.contacts.email"
+								:href="`mailto:${offer.contacts.phone1}`"
+								class="text-blue-600 hover:text-blue-900"
+							>
+								<MailIcon class="h-5 w-5 text-blue-500" />
+							</a>
+							<a
+								v-if="offer.contacts.facebook"
+								target="_blank"
+								:href="offer.contacts.facebook"
+								class="text-blue-600 hover:text-blue-900"
+							>
+								<IconFB />
+							</a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</div>
 </template>
