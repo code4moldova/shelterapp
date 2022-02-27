fd082fa40ce36a45bdc5ee1af41c0c273be5aaee
Sergiu <synthezys@gmail.com>
2022-02-27 17:07:53 +0200

    feat: list

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,50 +2,96 @@
 	<div class="flex flex-col">
 		<div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
 			<div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-				<div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+				<div
+					class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"
+				>
 					<table class="min-w-full divide-y divide-gray-200">
 						<thead class="bg-gray-50">
-						<tr>
-							<th scope="col" class="cursor-pointer px-6 py-3 text-left text-xs hover:bg-blue-200 font-medium text-gray-600 uppercase tracking-wider">Название</th>
-							<th scope="col" class="cursor-pointer px-6 py-3 text-left text-xs hover:bg-blue-200 font-medium text-gray-600 uppercase tracking-wider">Регион</th>
-							<th scope="col" class="cursor-pointer px-6 py-3 text-left text-xs hover:bg-blue-200 font-medium text-gray-600 uppercase tracking-wider">Локация</th>
-							<th scope="col" class="cursor-pointer px-6 py-3 text-left text-xs hover:bg-blue-200 font-medium text-gray-600 uppercase tracking-wider">Тип помощи</th>
-							<th scope="col" class="relative px-6 py-3">
-								<span class="sr-only">Edit</span>
-							</th>
-						</tr>
+							<tr>
+								<th
+									scope="col"
+									class="cursor-pointer px-6 py-3 text-left text-xs hover:bg-blue-200 font-medium text-gray-600 uppercase tracking-wider bg-gray-200"
+								>
+									Название
+								</th>
+								<th
+									scope="col"
+									class="cursor-pointer px-6 py-3 text-left text-xs hover:bg-blue-200 font-medium text-gray-600 uppercase tracking-wider bg-gray-200"
+								>
+									Регион
+								</th>
+								<th
+									scope="col"
+									class="cursor-pointer px-6 py-3 text-left text-xs hover:bg-blue-200 font-medium text-gray-600 uppercase tracking-wider bg-gray-200"
+								>
+									Локация
+								</th>
+								<th
+									scope="col"
+									class="cursor-pointer px-6 py-3 text-left text-xs hover:bg-blue-200 font-medium text-gray-600 uppercase tracking-wider bg-gray-200"
+								>
+									Тип помощи
+								</th>
+								<th scope="col" class="relative px-6 py-3 bg-gray-200">
+									<span class="sr-only">Edit</span>
+								</th>
+							</tr>
 						</thead>
 						<tbody class="bg-white divide-y divide-gray-200">
-						<tr v-for="offer in offers" :key="offer.id">
-							<td class="px-6 py-4 whitespace-nowrap">
-								<div class="flex items-center">
-									<div class="flex-shrink-0 h-10 w-10">
-										<img class="h-10 w-10 rounded-full" :src="offer.image" alt="" />
-									</div>
-									<div class="ml-4">
-										<div class="text-sm font-medium text-gray-900">
-											{{ offer.name }}
-										</div>
-										<div class="text-sm text-gray-500">
-											{{ offer.email }}
+							<tr v-for="offer in offers" :key="offer.id">
+								<td class="px-6 py-4 whitespace-nowrap">
+									<div class="flex items-center">
+										<!--									<div class="flex-shrink-0 h-10 w-10">-->
+										<!--										<img class="h-10 w-10 rounded-full" :src="offer.image" alt="" />-->
+										<!--									</div>-->
+										<div>
+											<div class="text-sm font-medium text-gray-900">
+												{{ offer.name }}
+											</div>
 										</div>
 									</div>
-								</div>
-							</td>
-							<td class="px-6 py-4 whitespace-nowrap">
-								<div class="text-sm text-gray-900">{{ offer.title }}</div>
-								<div class="text-sm text-gray-500">{{ offer.department }}</div>
-							</td>
-							<td class="px-6 py-4 whitespace-nowrap">
-								<span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"> Active </span>
-							</td>
-							<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-								{{ offer.role }}
-							</td>
-							<td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-								<a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-							</td>
-						</tr>
+								</td>
+								<td class="px-6 py-4 whitespace-nowrap">
+									<div class="text-sm text-gray-900">{{ offer.region }}</div>
+								</td>
+								<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+									<div class="text-sm text-gray-500">{{ offer.location }}</div>
+								</td>
+								<td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+									<div class="text-sm text-gray-500">{{ offer.helpType }}</div>
+								</td>
+								<td class="flex items-center text-sm py-4 px-6">
+									<a
+										v-if="offer.contacts.phone1"
+										:href="`tel:${offer.contacts.phone1}`"
+										class="text-blue-600 hover:text-blue-900"
+									>
+										<PhoneIcon class="h-5 w-5 text-blue-500" />
+									</a>
+									<a
+										v-if="offer.contacts.phone2"
+										:href="`tel:${offer.contacts.phone2}`"
+										class="text-blue-600 hover:text-blue-900"
+									>
+										<PhoneIcon class="h-5 w-5 text-blue-500" />
+									</a>
+									<a
+										v-if="offer.contacts.email"
+										:href="`mailto:${offer.contacts.phone1}`"
+										class="text-blue-600 hover:text-blue-900"
+									>
+										<MailIcon class="h-5 w-5 text-blue-500" />
+									</a>
+									<a
+										v-if="offer.contacts.facebook"
+										target="_blank"
+										:href="offer.contacts.facebook"
+										class="text-blue-600 hover:text-blue-900"
+									>
+										<IconFB />
+									</a>
+								</td>
+							</tr>
 						</tbody>
 					</table>
 				</div>
@@ -55,102 +101,122 @@
 </template>
 
 <script lang="ts">
+import { PhoneIcon, MailIcon } from "@heroicons/vue/solid";
+import IconFB from "../icons/IconFB.vue";
+import { ref } from "vue";
 type Offer = {
-	id_point: number,
-	type: string,
-	name: string,
-	region: string,
-	location: string,
-	helpType: string,
-	description?: string,
+	id: number;
+	type: string;
+	name: string;
+	region: string;
+	location: string;
+	helpType: string;
+	description?: string;
 	contacts: {
-		phone1: string,
-		phone2?: string,
-		facebook?: string,
-		email?: string,
-	},
+		phone1: string;
+		phone2?: string;
+		facebook?: string;
+		email?: string;
+	};
 	secondary?: {
-		address?: string,
-		address2?: string,
-	}
-}
-
+		address?: string;
+		address2?: string;
+	};
+};
 
 export default {
-	name: 'OfferList',
+	name: "OfferList",
+	components: {
+		IconFB,
+		PhoneIcon,
+		MailIcon,
+	},
 	setup() {
-		const offers: Offer[] = [
+		const initialOffers: Offer[] = [
 			{
 				id: 0,
-				name: 'Jane Cooper',
-				region: 'Regional Paradigm Technician',
-				location: 'Optimization',
-				helpType: 'Admin',
-				description: 'jane.cooper@example.com',
+				type: "type",
+				name: "Offer name",
+				region: "Chisinau",
+				location: "Centru",
+				helpType: "Medicamente",
 				contacts: {
-					phone1: '+1 (917) 593-3258',
-					phone2: '+1 (917) 593-3258',
-					facebook: 'https://www.facebook.com/jane.cooper',
-					email: 'example@example.com'
+					phone1: "+1 (917) 593-3258",
+					phone2: "+1 (917) 593-3258",
+					facebook: "https://www.facebook.com/jane.cooper",
+					email: "example@example.com",
 				},
 				secondary: {
-					address: '123 Main St',
-					address2: 'Suite 200'
-				}
+					address: "123 Main St",
+					address2: "Suite 200",
+				},
 			},
 			{
 				id: 1,
-				name: 'Jane Cooper',
-				region: 'Regional Paradigm Technician',
-				location: 'Optimization',
-				helpType: 'Admin',
-				description: 'jane.cooper@example.com',
+				type: "type",
+				name: "Offer name",
+				region: "Chisinau",
+				location: "Centru",
+				helpType: "Medicamente",
 				contacts: {
-					phone1: '+1 (917) 593-3258',
-					facebook: 'https://www.facebook.com/jane.cooper',
-					email: 'example@example.com'
+					phone1: "+1 (917) 593-3258",
+					phone2: "+1 (917) 593-3258",
+					facebook: "https://www.facebook.com/jane.cooper",
+					email: "example@example.com",
+				},
+				secondary: {
+					address: "123 Main St",
+					address2: "Suite 200",
 				},
 			},
 			{
 				id: 2,
-				name: 'Jane Cooper',
-				region: 'Regional Paradigm Technician',
-				location: 'Optimization',
-				helpType: 'Admin',
-				description: 'jane.cooper@example.com',
+				type: "type",
+				name: "Offer name",
+				region: "Chisinau",
+				location: "Centru",
+				helpType: "Medicamente",
 				contacts: {
-					phone1: '+1 (917) 593-3258',
-					phone2: '+1 (917) 593-3258',
-					facebook: 'https://www.facebook.com/jane.cooper',
-					email: 'example@example.com'
+					phone1: "+1 (917) 593-3258",
+					phone2: "+1 (917) 593-3258",
+					facebook: "https://www.facebook.com/jane.cooper",
+					email: "example@example.com",
+				},
+				secondary: {
+					address: "123 Main St",
+					address2: "Suite 200",
 				},
 			},
 			{
 				id: 3,
-				name: 'Jane Cooper',
-				region: 'Regional Paradigm Technician',
-				location: 'Optimization',
-				helpType: 'Admin',
-				description: 'jane.cooper@example.com',
+				type: "type",
+				name: "Offer name",
+				region: "Chisinau",
+				location: "Centru",
+				helpType: "Medicamente",
 				contacts: {
-					phone1: '+1 (917) 593-3258',
-					facebook: 'https://www.facebook.com/jane.cooper',
-					email: 'example@example.com'
+					phone1: "+1 (917) 593-3258",
+					phone2: "+1 (917) 593-3258",
+					facebook: "https://www.facebook.com/jane.cooper",
+					email: "example@example.com",
 				},
 				secondary: {
-					address: '123 Main St',
-					address2: 'Suite 200'
-				}
+					address: "123 Main St",
+					address2: "Suite 200",
+				},
 			},
 			// More people...
-		]
-		function filterBy(field) {
+		];
 
-		}
+		const offers = ref<Offer[]>(initialOffers);
+
+		// function filterBy(field) {
+		//
+		// }
 		return {
 			offers,
-			filterBy
-		}
+			//filterBy,
+		};
 	},
-}
+};
 </script>
